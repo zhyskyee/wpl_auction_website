eecb39521e8c1255a1a7767372e852c0804482e9
Wayne Zhang <zhyskyee1125@gmail.com>
2018-12-07 19:35:10 -0600

    [Modify] Set A server communicate with B
    TBD

--- auctionweb/src/main/java/utd/wpl/controller/UserController.java
+++ auctionweb/src/main/java/utd/wpl/controller/UserController.java
@@ -1,28 +1,63 @@
 package utd.wpl.controller;
 
-import java.util.Date;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import utd.wpl.pojo.User;
-import utd.wpl.service.UserService;
-import com.mysql.jdbc.StringUtils;
+
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+//import com.fasterxml.jackson.databind.util.JSONPObject;
 
 @Controller
 public class UserController {
+		
+		class Result {
+		String answer;
+		public String getAnswer() {
+			return answer;
+		}
+		public void setAnswer(String answer) {
+			this.answer = answer;
+		}
+		public Result() {
+			// TODO Auto-generated constructor stub
+			super();
+		}
+	}
 
-	@Autowired
-	private UserService userService;
 
 	// 显示主页
 	@RequestMapping("/")
@@ -31,9 +66,15 @@ public class UserController {
 	}
 	
 	@GetMapping("/profile") 
-	public String showProfile() {
-		return "profile";
-	} 
+	public void showProfile(User user) {
+	
+		HttpRequest httpRequest = HttpRequest.get("http://localhost:8989/user/{username}", null, Boolean.TRUE);
+		String result = httpRequest.body();
+		
+		System.out.println(result);
+
+	}
+	
 	//for now
 	@GetMapping("/order") 
 	public String showOrder() {
@@ -55,50 +96,109 @@ public class UserController {
 	public String showRegister() {
 		return "register";
 	}
-
+	
 	// 处理登录请求
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(User user, Model model, HttpServletRequest request) {
-		//查询用户是否存在
-		User findUser = userService.findUser(user.getUsername());
-		
-		//把密码转成MD5加密后的
-		String passwdMD5 = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
-		if (findUser == null || !passwdMD5.equals(findUser.getPassword())) {
-			// 登录失败
-			returnLoginData(model, user,"Username does not existed or password incorrect!");
-			return "index";
+	@RequestMapping(value = "/login", method=RequestMethod.POST)
+	public void login(@RequestParam("username") String un, @RequestParam("password") String pswd) {
+		JSONObject object = new JSONObject();
+		object.put("username", un);
+		object.put("password", pswd);
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+
+		ResponseHandler<String> responseHandler = new BasicResponseHandler();
+		try {
+			// 第一步：创建HttpClient对象
+			httpClient = HttpClients.createDefault();
+			// 第二步：创建httpPost对象
+			HttpPost httpPost = new HttpPost("http://localhost:8989/user/login");
+
+			// 第三步：给httpPost设置JSON格式的参数
+			StringEntity requestEntity = new StringEntity(object.toString(), "utf-8");
+
+			requestEntity.setContentEncoding("UTF-8");
+			httpPost.setHeader("Content-type", "application/json");
+			httpPost.setEntity(requestEntity);
+
+			// 第四步：发送HttpPost请求，获取返回值
+			String reString = httpClient.execute(httpPost, responseHandler); // 调接口获取返回值时，必须用此方法
+			// 。。。。。。。。
+			System.out.println("Ressutl:" + reString);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		//登录成功
-		//更新用户最后访问时间
-		findUser.setLast_visit(new Date());
-		userService.updateLastVisit(findUser);
-		//设置username属性，方便jsp页面获取
-		model.addAttribute("username", findUser.getUsername());
-		model.addAttribute("message", "Login successfully!");
-		// 设置用户信息保存到session
-		request.getSession().setAttribute("user", findUser);
-		//返回主页
-		return "index";
+
+		finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
+	
+	
 
 	// 用户注册
 	@RequestMapping(value = "/register/do", method = RequestMethod.POST)
-	public String register(User user, String confirmPass, Model model) {
-		// 查询用户是否存在
-		User findUser = userService.findUser(user.getUsername());
-		if (findUser != null) {
-			returnRegisterData(model, user, confirmPass, "用户名已被注册！");
-			return "register";
+	public ResponseEntity<Result> register(User user,String confirmPass) throws IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		JSONObject object = new JSONObject();
+		object.put("username", user.getUsername());
+		object.put("password", user.getPassword());
+		object.put("email", user.getEmail());
+		object.put("phone", user.getPhone());
+		object.put("photo", user.getPhoto());
+//		System.out.println("================>");
+		try {
+			// 第一步：创建HttpClient对象
+			httpClient = HttpClients.createDefault();
+			/*
+			 * 添加参数到URL的尾巴
+			 */
+			URIBuilder builder = new URIBuilder("http://localhost:8989/user/register");
+			builder.addParameter("confirmPass", confirmPass);
+			// 第二步：创建httpPost对象
+			HttpPost httpPost = new HttpPost(builder.build());
+			// 第三步：给httpPost设置JSON格式的参数
+			StringEntity requestEntity = new StringEntity(object.toString());
+			requestEntity.setContentEncoding("UTF-8");
+			httpPost.setHeader("Content-type", "application/json");
+			httpPost.setEntity(requestEntity);
+			// 第四步：发送HttpPost请求，获取返回值
+			HttpResponse hr = httpClient.execute(httpPost); // responseHandler调接口获取返回值时，必须用此方法
+			ResponseHandler<String> responseHandler = new BasicResponseHandler();
+			// Response Body
+			String responseBody = responseHandler.handleResponse(hr);
+			int statusCode = hr.getStatusLine().getStatusCode();
+//			System.out.println("statusCode:::" + statusCode+"     resp:"+responseBody);
+			
+			Gson gson = new Gson();
+			Result result;
+			try {
+				result = gson.fromJson(responseBody, Result.class);
+			} catch (JsonSyntaxException ex) {
+				// TODO: handle exception
+				ex.printStackTrace();
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			}
+			
+//			result.setAnswer();
+			return new ResponseEntity<Result>(result, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		model.addAttribute("username", user.getUsername());
-		//补全pojo属性
-		user.setLast_visit(new Date());
-		//密码使用MD5加密
-		user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes()));
-		//添加新用户
-		userService.addUser(user);
-		return "success";
+
+		finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 	}
 	
 	//用户注销
@@ -110,6 +210,8 @@ public class UserController {
 		return "redirect:/";
 	}
 	
+	
+/*	
 	//回显登录数据
 	public void returnLoginData(Model model,User user,String meg) {
 		model.addAttribute("username",user.getUsername());
@@ -124,4 +226,7 @@ public class UserController {
 		model.addAttribute("confirmPass",confirmPass);
 		model.addAttribute("message",meg);
 	}
+
+*/
 }
+
